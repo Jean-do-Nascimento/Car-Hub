fbfe192926cbec559d397200e44f94fce72471af
Jean do Nascimento <jeandonascmento@gmail.com>
2023-09-08 00:13:00 -0300

    first commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,9 +5,13 @@ import CustomButton  from "./CustomButton";
 
 const Hero = () => {
     const handleScroll= () => {
+        const nextSection = document.getElementById("discover");
 
+        if (nextSection) {
+          nextSection.scrollIntoView({ behavior: "smooth" });
+        }
     }
-
+    
   return (
     <div className="hero">
         <div className="flex-1 pt-36 padding-x">
