5c851fbeac6fa31458ff70cd14910aa0ceddf4fc
Jean do Nascimento <jeandonascmento@gmail.com>
2024-04-21 19:04:38 -0300

    first commit

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -35,7 +35,7 @@ const CarCard = ({car}: CarCardProps) => {
             </span>
         </p>
         <div className="relative w-full h-40 my-3 object-contain">
-            <Image src={generateCarImageUrl(car)} alt="car model" fill priority className="object-contain" />
+            <Image src={generateCarImageUrl(car, "01")} alt="car model" fill priority className="object-contain" />
         </div>
 
         <div className="relative flex w-full mt-2">
